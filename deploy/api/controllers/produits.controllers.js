0cce28336acc9f519de9deef69a1f5c2c706a5d3
Tompr1 <tomprieto6@gmail.com>
2025-01-11 00:27:38 +0100

    test

--- deploy/api/controllers/produits.controllers.js
+++ deploy/api/controllers/produits.controllers.js
@@ -1,4 +1,4 @@
-exports.getProduits = (req, res) => {
+exports.get = (req, res) => {
     const produits = [
         {"ref": "Orange", "prix": 45, "desc": "Je suis une orange", "stock": 10},
         {"ref": "Banane", "prix": 56, "desc": "Banane", "stock": 5},
